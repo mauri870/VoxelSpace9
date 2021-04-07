32ad17423455faab942e49188b1c0438e2fc25cf
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2021-04-07 08:16:25 -0300

    fix: reorder includes

--- VoxelSpace.c
+++ VoxelSpace.c
@@ -1,9 +1,9 @@
+#include <u.h>
+#include <libc.h>
+#include <stdio.h>
 #include <draw.h>
 #include <event.h>
-#include <libc.h>
 #include <memdraw.h>
-#include <stdio.h>
-#include <u.h>
 
 /* Colormap image file */
 Memimage *cmapim;
